1792da26ffd84d75930e092b52d7bf67cb8c007f
Jimmy Liu <jliu8880@gmail.com>
2026-04-05 20:26:15 -0400

    use generics for arguments for stronger typing

--- src/main/java/oop/project/library/argument/Argument.java
+++ src/main/java/oop/project/library/argument/Argument.java
@@ -1,19 +1,19 @@
 package oop.project.library.argument;
 
-public class Argument {
+public class Argument<T> {
     // represents an argument for a command. EX: "add 1 2" has two arguments "1" and "2"
     // arguments currently a name and a type that can be specified
     // for methods, return "this" to support chaining of methods
 
     String name;
-    Class<?> type;
+    Class<T> type;
 
-    public Argument(String name) {
+    public Argument(String name, Class<T> type) {
         this.name = name;
-        this.type = String.class; // default type is String
+        this.type = type;
     }
 
-    public Argument setType(Class<?> type) {
+    public Argument<T> type(Class<T> type) {
         this.type = type;
         return this; // for chaining
     }
--- src/main/java/oop/project/library/argument/ArgumentParser.java
+++ src/main/java/oop/project/library/argument/ArgumentParser.java
@@ -1,6 +1,10 @@
 package oop.project.library.argument;
 
+import oop.project.library.input.BasicArgs;
+import oop.project.library.input.Input;
+
 import java.util.ArrayList;
+import java.util.Map;
 
 public class ArgumentParser {
     /*
@@ -8,18 +12,34 @@ public class ArgumentParser {
     like python's argparse, constructor takes in a program/command name
     add_arg method will support arguments for that program/command
     in the argument parser, keep a list of type Arguments?
+    TODO allow custom parsing (ie LocalDate's parse method)
+    allow for refinement of arguments (range)
     */
 
     String programName;
-    ArrayList<Argument> arguments = new ArrayList<>();
+    ArrayList<Argument<?>> arguments = new ArrayList<>();
 
     public ArgumentParser(String programName) {
         this.programName = programName;
     }
 
-    public void addArg(String name) {
+    public<T> Argument<T> addArg(String name, Class<T> type) {
         // add an argument with the given name and default type of String
-        arguments.add(new Argument(name));
+        Argument<T> argument = new Argument<>(name, type);
+        arguments.add(argument);
+        return argument;
     }
 
+    // overloaded addArg for default type of String
+    public Argument<String> addArg(String name) {
+        return addArg(name, String.class);
+    }
+
+    public Map parseArgs(String arguments) throws RuntimeException {
+        Input lexer = new Input(arguments);
+
+        BasicArgs args = lexer.parseBasicArgs();
+
+        return null;
+    }
 }
